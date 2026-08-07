3733ce50d13405ee19d647e43bbe7a31d89932fe
Developer <developer@example.com>
2026-08-06 20:03:02 -0500

    fix: update valid Algolia search API key in algolia_service.dart

--- lib/services/algolia_service.dart
+++ lib/services/algolia_service.dart
@@ -10,7 +10,7 @@ class AlgoliaService {
   AlgoliaService._internal();
 
   final String _appId = 'P2CJMQDDSH';
-  final String _apiKey = '99ec76cf710a0324bccd1f008514eb36';
+  final String _apiKey = '4aa72340abeb49d79d888cc3271c23b1';
   static const String _adminApiKey = '4aa72340abeb49d79d888cc3271c23b1';
 
   late SearchClient _client;
