4897abc55a981c610cc17fc364308f0316191ba0
Developer <developer@example.com>
2026-08-07 17:37:39 -0500

    Fix Swift WMO crash on iOS Release: explicitly reference AudioplayersDarwinPlugin.self in AppDelegate.swift and force singlefile Swift compilation mode in Podfile, bump version to 1.1.18+28

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import audioplayers_darwin
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Previene la eliminación de metadatos de Swift por optimización Whole Module en Release
+    _ = AudioplayersDarwinPlugin.self
+
     GMSServices.provideAPIKey("AIzaSyCKB9H2-C7C2lbyyl1tp3R5rKoHyGosfII")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
