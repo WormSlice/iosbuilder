b83648b310e37cb76a4dd84a4ccffddbff3bdcb0
Developer <developer@example.com>
2026-08-09 18:31:11 -0500

    Retain CameraPlugin Swift metadata in AppDelegate.swift to prevent swift_getObjectType crash

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,11 +2,6 @@ import Flutter
 import UIKit
 import GoogleMaps
 import camera_avfoundation
-import app_links
-import sign_in_with_apple
-import record_ios
-import speech_to_text
-import geolocator_apple
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -16,13 +11,8 @@ import geolocator_apple
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyCKB9H2-C7C2lbyyl1tp3R5rKoHyGosfII")
 
-    // Registrar plugins de Swift directamente en Swift con metadatos de clase validos para evitar SIGSEGV en swift_getObjectType
-    if let r = self.registrar(forPlugin: "AppLinksIosPlugin") { AppLinksIosPlugin.register(with: r) }
-    if let r = self.registrar(forPlugin: "CameraPlugin") { CameraPlugin.register(with: r) }
-    if let r = self.registrar(forPlugin: "SignInWithApplePlugin") { SignInWithApplePlugin.register(with: r) }
-    if let r = self.registrar(forPlugin: "RecordIosPlugin") { RecordIosPlugin.register(with: r) }
-    if let r = self.registrar(forPlugin: "SpeechToTextPlugin") { SpeechToTextPlugin.register(with: r) }
-    if let r = self.registrar(forPlugin: "GeolocatorPlugin") { GeolocatorPlugin.register(with: r) }
+    // Retain CameraPlugin Swift class metadata to prevent swift_getObjectType launch crash
+    _ = CameraPlugin.self
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
