092b216557e7bd877106fbac4d9654a7e0f498c4
Developer <developer@example.com>
2026-08-19 14:51:20 -0500

    fix(ios): use static linkage for CocoaPods to eliminate Swift dynamic class metadata launch crash in CameraPlugin and plugins

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import Flutter
 import UIKit
 import GoogleMaps
-import camera_avfoundation
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,9 +9,6 @@ import camera_avfoundation
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyCKB9H2-C7C2lbyyl1tp3R5rKoHyGosfII")
-
-    // Retain CameraPlugin Swift class metadata to prevent swift_getObjectType launch crash
-    _ = CameraPlugin.self
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
