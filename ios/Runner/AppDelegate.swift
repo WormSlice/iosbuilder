0c5416a06f9a1c2481ffe311d868dd19f1f4c05f
Developer <developer@example.com>
2026-08-19 21:12:11 -0500

    fix(ios): initialize FlutterAppDelegate engine before GeneratedPluginRegistrant; bump version to 1.1.23+44

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,8 @@ import GoogleMaps
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyCKB9H2-C7C2lbyyl1tp3R5rKoHyGosfII")
 
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return result
   }
 }
